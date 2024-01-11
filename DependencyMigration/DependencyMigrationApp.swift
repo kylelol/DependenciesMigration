7a79f1e1687eba8e1e8d19ad349fe13ec6185e14
Kyle Kirkland <Kyle.kirkland0528@gmail.com>
2024-01-11 16:34:26 -0500

    Initial Commit

---
+++ DependencyMigration/DependencyMigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyMigrationApp.swift
+//  DependencyMigration
+//
+//  Created by Kyle Kirkland on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DependencyMigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
